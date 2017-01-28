ebf21d04a87961f6eca7a7e853efe2d2c6a35b5c
George Higbie <gehigbie@gmail.com>
2017-01-27 17:17:35 -0700

    created getter and setters

---
+++ src/Account.java
@@ -0,0 +1,60 @@
+/**
+ * Created by georgehigbie on 1/27/17.
+ */
+public class Account {
+
+    private String number; //this could be an number, but a string may be better
+    private double balance;
+    private String customerFirstName;
+    private String customerLastName;
+    private String emailAddress;
+    private String phoneNumber;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
