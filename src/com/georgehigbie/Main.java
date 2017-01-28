bc3dff71c3b62fe4e5abbcdac6ef6975d369131a
George Higbie <gehigbie@gmail.com>
2017-01-27 17:41:16 -0700

    tested account class

--- src/com/georgehigbie/Main.java
+++ src/com/georgehigbie/Main.java
@@ -6,5 +6,16 @@ public class Main {
 	// write your code here
 
         Account bobsAccount = new Account();
+        bobsAccount.withdrawal(200.0);
+
+        bobsAccount.deposit(100.0);
+        bobsAccount.withdrawal(50.0);
+
+        bobsAccount.deposit(51.0);
+        bobsAccount.withdrawal(30.0);
+
+        Account tomsAccount = new Account();
+        tomsAccount.setBalance(500.00);
+        tomsAccount.getBalance();
     }
 }
