28d9d8e4e8e64ba3a4be36cd7d6985836875386a
George Higbie <gehigbie@gmail.com>
2017-01-27 21:53:06 -0700

    added new constructor

--- src/com/georgehigbie/VIPCustomer.java
+++ src/com/georgehigbie/VIPCustomer.java
@@ -7,16 +7,26 @@ public class VIPCustomer {
 
     private String firstName;
     private String lastName;
-    private String emailAddres;
+    private String emailAddress;
     private double creditLimit;
 
-    public VIPCustomer(String firstName, String lastName, String emailAddres, double creditLimit){
+    public VIPCustomer(String firstName, String lastName, String emailAddress, double creditLimit){
         this.firstName = firstName;
         this.lastName = lastName;
-        this.emailAddres = emailAddres;
+        this.emailAddress = emailAddress;
         this.creditLimit = creditLimit;
     }
 
+    public VIPCustomer(String firstName, String lastName, String emailAddress){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.creditLimit = 0.0;
+    }
+
+    public VIPCustomer(){
+        this("John", "Doe", "jg@email.com", 0.0);
+    }
 
 
 }
